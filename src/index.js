979bd0f366131a685f20ae1c3262ecf08d18e820
Ruben Ollesch <ruben.ollesch@protonmail.com>
2018-08-28 14:21:38 +0200

    try/catch for chayns.ready

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-/* eslint no-new: 0 */
-
 import Form from './components/formContainer/formSetup';
 import Search from './components/searchContainer/searchHandler';
 
@@ -7,11 +5,15 @@ import searchConfig from './constants/searchConfig';
 import formConfig from './constants/formConfig';
 
 const init = async () => {
-    await chayns.ready;
+    try {
+        await chayns.ready;
 
-    const tapp = document.querySelector('.tapp__content');
-    tapp.appendChild(new Form(formConfig));
-    tapp.appendChild(new Search(searchConfig));
+        const tapp = document.querySelector('.tapp__content');
+        tapp.appendChild(new Form(formConfig));
+        tapp.appendChild(new Search(searchConfig));
+    } catch (error) {
+        console.error(error);
+    }
 };
 
 init();
